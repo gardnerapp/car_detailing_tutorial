529dcfb8be1807d7f9f369dcde80af2ccdf39299
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2022-06-24 13:42:15 -0400

    Begin adding callbacks

--- wash.dart
+++ wash.dart
@@ -3,10 +3,16 @@ import 'package:car_detailing/shared/book_appointment.dart';
 import 'package:car_detailing/shared/instruction_text.dart';
 import 'package:flutter/material.dart';
 
-// Adding call back functions & options tiles
+// TODO Debug DateTime and TimeOfDay selection error
 
-class Wash extends StatelessWidget {
+class Wash extends StatefulWidget {
   const Wash({Key key}) : super(key: key);
+
+  @override
+  _WashState createState() => _WashState();
+}
+
+class _WashState extends State<Wash> {
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +24,14 @@ class Wash extends StatelessWidget {
             spacer(),
             instructionText("Our Soaps are imported from all 4 corners of the "
                 "7 seas they will leave your car shiny and looking brand new\n"),
-            instructionText("For an extra Satoshi you can have your car smelling like roses"),
-
+            instructionText(
+                "For an extra Satoshi you can have your car smelling like roses"),
             spacer(),
-            Divider(height: 5.0, thickness: 5.0, color: Theme.of(context).primaryColor,),
+            Divider(
+              height: 5.0,
+              thickness: 5.0,
+              color: Theme.of(context).primaryColor,
+            ),
             SizedBox(height: 25.0),
             BookAppointment(
 
